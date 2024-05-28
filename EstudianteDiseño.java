02008cf07d7eb29080ab8d59a2ad7532363649ca
kcanov <167874163+kcanov@users.noreply.github.com>
2024-05-27 23:01:16 -0500

    Update EstudianteDiseño.java

--- EstudianteDiseño.java
+++ EstudianteDiseño.java
@@ -1,39 +1,96 @@
+package practica;
+
 public class EstudianteDiseño extends Estudiante {
-    private String modalidadEstudio;
-    private int cantidadAsignaturas;
+    private String modalidad;
+    private int asignaturas;
+    private String marca;
+    private String almacenamiento;
+    private float tamaño;
+    private float peso;
+    private float precio;
 
-    public EstudianteDiseño(String cedula, String nombre, String apellido, String telefono, String serial, String modalidadEstudio, int cantidadAsignaturas) {
+    public EstudianteDiseño(String cedula, String nombre, String apellido, String telefono, String serial, String modalidad, int asignaturas, String marca, String almacenamiento, float tamaño, float peso, float precio) {
         super(cedula, nombre, apellido, telefono, serial);
-        this.modalidadEstudio = modalidadEstudio;
-        this.cantidadAsignaturas = cantidadAsignaturas;
+        this.modalidad = modalidad;
+        this.asignaturas = asignaturas;
+        this.marca = marca;
+        this.almacenamiento = almacenamiento;
+        this.tamaño = tamaño;
+        this.peso = peso;
+        this.precio = precio;
     }
 
-    public String getModalidadEstudio() {
-        return modalidadEstudio;
+    public String getModalidad() {
+        return modalidad;
     }
 
-    public void setModalidadEstudio(String modalidadEstudio) {
-        this.modalidadEstudio = modalidadEstudio;
+    public void setModalidad(String modalidad) {
+        this.modalidad = modalidad;
     }
 
-    public int getCantidadAsignaturas() {
-        return cantidadAsignaturas;
+    public int getAsignaturas() {
+        return asignaturas;
     }
 
-    public void setCantidadAsignaturas(int cantidadAsignaturas) {
-        this.cantidadAsignaturas = cantidadAsignaturas;
+    public void setAsignaturas(int asignaturas) {
+        this.asignaturas = asignaturas;
     }
-    
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAlmacenamiento() {
+        return almacenamiento;
+    }
+
+    public void setAlmacenamiento(String almacenamiento) {
+        this.almacenamiento = almacenamiento;
+    }
+
+    public float getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(float tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
     @Override
     public String toString() {
-        return "EstudianteDiseno{" +
-                "cedula='" + getCedula() + '\'' +
+        return "EstudianteDiseño{" +
+                "modalidad='" + modalidad + '\'' +
+                ", asignaturas=" + asignaturas +
+                ", marca='" + marca + '\'' +
+                ", almacenamiento='" + almacenamiento + '\'' +
+                ", tamaño=" + tamaño +
+                ", peso=" + peso +
+                ", precio=" + precio +
+                ", cedula='" + getCedula() + '\'' +
                 ", nombre='" + getNombre() + '\'' +
                 ", apellido='" + getApellido() + '\'' +
                 ", telefono='" + getTelefono() + '\'' +
                 ", serial='" + getSerial() + '\'' +
-                ", modalidad='" + getModalidadEstudio() + '\'' +
-                ", asignaturas=" + getCantidadAsignaturas() +
                 '}';
     }
 }
