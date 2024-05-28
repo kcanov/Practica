fa7de7761f6c8eb6e97fbfa2a1333d5b95812613
kcanov <167874163+kcanov@users.noreply.github.com>
2024-05-27 23:02:05 -0500

    Update EstudianteIngenieria.java

--- EstudianteIngenieria.java
+++ EstudianteIngenieria.java
@@ -1,39 +1,98 @@
+package practica;
+
 public class EstudianteIngenieria extends Estudiante {
-    private int numSemestre;
-    private float promedioAcumulado;
+    private int semestre;
+    private float promedio;
+    private String sistemaOperativo;
+    private String procesador;
+    private String marca;
+    private float tamaño;
+    private float precio;
 
-    public EstudianteIngenieria(String cedula, String nombre, String apellido, String telefono, String serial, int numSemestre, float promedioAcumulado) {
+    public EstudianteIngenieria(String cedula, String nombre, String apellido, String telefono, String serial, int semestre, float promedio, String sistemaOperativo, String procesador, String marca, float tamaño, float precio) {
         super(cedula, nombre, apellido, telefono, serial);
-        this.numSemestre = numSemestre;
-        this.promedioAcumulado = promedioAcumulado;
+        this.semestre = semestre;
+        this.promedio = promedio;
+        this.sistemaOperativo = sistemaOperativo;
+        this.procesador = procesador;
+        this.marca = marca;
+        this.tamaño = tamaño;
+        this.precio = precio;
     }
 
-    public int getNumeroSemestre() {
-        return numSemestre;
+    public int getSemestre() {
+        return semestre;
     }
 
-    public void setNumeroSemestre(int numeroSemestre) {
-        this.numSemestre = numSemestre;
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
     }
 
-    public float getPromedioAcumulado() {
-        return promedioAcumulado;
+    public float getPromedio() {
+        return promedio;
     }
 
-    public void setPromedioAcumulado(float promedioAcumulado) {
-        this.promedioAcumulado = promedioAcumulado;
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
     }
-    
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    public String getProcesador() {
+        return procesador;
+    }
+
+    public void setProcesador(String procesador) {
+        this.procesador = procesador;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public float getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(float tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+
+
     @Override
     public String toString() {
         return "EstudianteIngenieria{" +
-                "cedula='" + getCedula() + '\'' +
+                "semestre=" + semestre +
+                ", promedio=" + promedio +
+                ", sistemaOperativo='" + sistemaOperativo + '\'' +
+                ", procesador='" + procesador + '\'' +
+                ", marca='" + marca + '\'' +
+                ", tamaño=" + tamaño +
+                ", precio=" + precio +
+                ", cedula='" + getCedula() + '\'' +
                 ", nombre='" + getNombre() + '\'' +
                 ", apellido='" + getApellido() + '\'' +
                 ", telefono='" + getTelefono() + '\'' +
                 ", serial='" + getSerial() + '\'' +
-                ", semestre=" + getNumeroSemestre() +
-                ", promedio=" + getPromedioAcumulado() +
                 '}';
     }
 }
